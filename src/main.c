e655634db4918dd8647448ff2df0077d9efb77d2
Amino75 <amin.feizi751381@gmail.com>
2026-08-04 17:33:52 +0000

    Reorganize include directory

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("\n");
+    printf("=====================================\n");
+    printf(" Intelligent Guard System\n");
+    printf("=====================================\n");
+    printf("Starting...\n");
+
+    return 0;
+}
